a72db641bd52a738a90c43a5e9ccceab29021396
dannyvassallo <danielvassallo87@gmail.com>
2016-03-22 16:47:15 -0400

    added username column and before filter for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
 
   # When you want to add username and/or name, check out:
   # http://devise.plataformatec.com.br/#strong-parameters
@@ -11,4 +13,10 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :all_heroes
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
