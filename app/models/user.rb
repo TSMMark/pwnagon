01612febecb881b6089085eb87050dfaddd7286c
dannyvassallo <danielvassallo87@gmail.com>
2016-03-22 16:51:54 -0400

    validates case insensitive uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_voter
+  validates_uniqueness_of :username, :case_sensitive => false
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
