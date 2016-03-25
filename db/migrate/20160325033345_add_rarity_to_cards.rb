5613eb103e69be454161c1d2c2208f9b2ba69f1d
TSMMark <mark.allen@vydia.com>
2016-03-25 18:59:10 -0400

    don't use model const in migration

--- db/migrate/20160325033345_add_rarity_to_cards.rb
+++ db/migrate/20160325033345_add_rarity_to_cards.rb
@@ -1,5 +1,5 @@
 class AddRarityToCards < ActiveRecord::Migration
   def change
-    add_column :cards, :rarity, :string, null: false, default: Card::DEFAULT_RARITY
+    add_column :cards, :rarity, :string, null: false, default: "common"
   end
 end
