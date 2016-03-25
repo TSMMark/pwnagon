94841585b7d1c6bf380edde9e06801ae3db92225
TSMMark <mark.allen@vydia.com>
2016-03-25 08:46:29 -0400

    initial crawler get info from infobox

--- lib/crawlers/cards.rb
+++ lib/crawlers/cards.rb
@@ -20,6 +20,7 @@ module Pwnagon
         visit "/Cards"
 
         cards = []
+        distinct_infobox_keys = []
 
         within("#mw-content-text") do
           all("li.gallerybox").each do |card_gallerybox|
@@ -29,10 +30,13 @@ module Pwnagon
             card_thumb_120 = img_node[:src]
             card_thumb_180, card_thumb_240 = parse_srcset(img_node[:srcset])
 
+            card_details_path = card_gallerybox.find(".thumb a")[:href]
+
             puts "Name: #{card_name.inspect}"
             puts "Thumb 120: #{card_thumb_120.inspect}"
             puts "Thumb 180: #{card_thumb_180.inspect}"
             puts "Thumb 240: #{card_thumb_240.inspect}"
+            puts "Details Path: #{card_details_path.inspect}"
 
             # TODO: Get 175, 263, 350 thumb sizes from each card detail page.
             # TODO: Get type, cost, affinity and other details from card detail page.
@@ -41,7 +45,8 @@ module Pwnagon
               name: card_name,
               thumb_120: card_thumb_120,
               thumb_180: card_thumb_180,
-              thumb_240: card_thumb_240
+              thumb_240: card_thumb_240,
+              details_path: card_details_path
             }
 
             cards << card
@@ -50,6 +55,31 @@ module Pwnagon
         end
 
         puts "Found #{cards.count} Cards..."
+
+        cards.each do |card|
+          visit card[:details_path]
+          within(".infobox.wikitable") do
+            img_node = find(".image img")
+            card_thumb_175 = img_node[:src]
+            card_thumb_263, card_thumb_350 = parse_srcset(img_node[:srcset] || "") # TODO: Better handle missing srcset?
+
+            card[:thumb_175] = card_thumb_175
+            card[:thumb_263] = card_thumb_263
+            card[:thumb_350] = card_thumb_350
+
+            # for each tr, if there are 2 tds in it, it's a key and a value. Use a mapper and coerce data.
+            infobox_data = get_infobox_key_value_pairs
+            distinct_infobox_keys |= infobox_data.keys
+            puts "distinct_infobox_keys: \n#{distinct_infobox_keys.to_yaml}\n\n"
+            puts "infobox_data: \n#{infobox_data.to_yaml}\n\n"
+
+            puts card.to_yaml
+            puts "\n.......\n"
+          end
+        end
+
+        puts "Found #{cards.count} Cards..."
+        puts "\n\nYAML:", cards.to_yaml
         puts "\n\nJSON:", cards.to_json
       end
 
@@ -61,6 +91,54 @@ module Pwnagon
         end
       end
 
+      def get_infobox_key_value_pairs
+        all("tr").each_with_object({}) do |tr, hash|
+          all_tds = tr.all("td")
+          next unless all_tds.size == 2 # One key + one value.
+          key = all_tds[0].text.tr(":", "")
+          value = all_tds[1].text
+          hash[key] = value
+        end
+      end
+
     end
   end
 end
+
+# All distinct_infobox_keys discovered:
+# TODO: Just dump most of these into a JSON column on each card record.
+# Special keys:
+#  - type (active / passive / upgrade) - split into upgrade vs equipment + trigger_type active|passive
+#  - affinity
+#  - cost (strip non-\d)
+#  - rarity (basic, common, uncommon)
+# Type
+# Affinity
+# Cost
+# Rarity
+
+
+# Energy Damage
+# Damage Bonus
+# Unique Passive
+# Max Health
+# Physical Damage
+# Crit Bonus
+# Max Movement Speed
+# Charges
+# Unique Active
+# Passive
+# Harvester Placement Time
+# Max Mana
+# Active
+# Cooldown Reduction
+# Cooldown
+# Crit Chance
+# Physical Armor
+# Energy Armor
+# Attack Speed
+# Physical Penetration
+# Lifesteal
+# Energy Penetration
+# Health Regeneration
+# Mana Renegeration
