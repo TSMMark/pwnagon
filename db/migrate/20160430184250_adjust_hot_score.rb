5cfc9178db48b681194b3f1c38df0552e6e69d84
TSMMark <mark.allen@vydia.com>
2016-04-30 15:01:27 -0400

    much less weight on hot_score

---
+++ db/migrate/20160430184250_adjust_hot_score.rb
@@ -0,0 +1,40 @@
+class AdjustHotScore < ActiveRecord::Migration
+  # http://inaka.net/blog/2015/03/25/hot-score-with-ruby-postgresql-and-elastic-part-1/
+  def up
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION
+        hot_score(ups integer, downs integer, date timestamp with time zone)
+        returns numeric as $$
+          select
+            round(
+              cast(
+                log(greatest(abs($1 - $2), 1)) *
+                sign($1 - $2) +
+                (date_part('epoch', $3) - 1134028003) / 4500000.0
+                as numeric
+              ),
+              7
+            )
+        $$ language sql immutable;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION
+        hot_score(ups integer, downs integer, date timestamp with time zone)
+        returns numeric as $$
+          select
+            round(
+              cast(
+                log(greatest(abs($1 - $2), 1)) *
+                sign($1 - $2) +
+                (date_part('epoch', $3) - 1134028003) / 45000.0
+                as numeric
+              ),
+              7
+            )
+        $$ language sql immutable;
+    SQL
+  end
+end
